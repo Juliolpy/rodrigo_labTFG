7130ae02c86f89bef65a767cc67ff2a96780115e
Julio <jcochenta@gmail.com>
2025-02-24 09:52:18 +0100

    cambios hechos para que el script funcione como programación orientada a objetos

--- src/Issue_2.py
+++ src/Issue_2.py
@@ -1,7 +1,17 @@
 import RNA
-# me la he inventado
-seq = "AUGUAGUAGUAUCUGUACUAGUGUAUGAUUCUAUGAUG"
+import sys
 
-(ss, mfe) = RNA.fold(seq)
+if len(sys.argv) != 2:
+    print("Inserte dos argumentos, de tal manera que:")
+    print(": python3 name_script.py [secuencia de ARN]")
 
-print("{}\n{} [ {:6.2f} ]".format(seq, ss, mfe))
+seq = sys.argv[1]
+
+# creamos el objeto fold compound
+
+fc = RNA.fold_compound(seq)
+
+# obtener su structura (ss) y su energía mínima (mfe)
+(ss, mfe) = fc.mfe()
+
+print(f"{seq}\n{ss} [ {mfe:6.2f} ] kcal/mol")
