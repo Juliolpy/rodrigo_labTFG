fd61901cf367742cf0c16445bddb1d6dc20e464f
Julio <jcochenta@gmail.com>
2025-03-05 17:34:43 +0100

    He entendido el problema que sucedia en el script de modsum al hacerlo pasar por un test, pero me sigue dando error ya que dice que no encuentra un módulo llamado src

---
+++ tests/test_modsum.py
@@ -0,0 +1,28 @@
+from src.modsum import modsum
+
+
+def test_mod2() -> None:
+    """
+    Comprueba que la suma en módulo 2 funciona.
+
+    Para ello voy a tomar como argumentos los números 24 y 67.
+    """
+    assert modsum(24, 67, 2) == 1
+
+def test_mod3() -> None:
+    """
+    Comprueba que la suma en módulo 3 funciona.
+
+    Para ello voy a tomar como argumentos los números 24 y 67.
+    """
+    assert modsum(24, 67, 3) == 1
+# aqui esta el error
+def test_normal_sum() -> None:
+    # entiendo que aquí querías decir en módulo normal, cuando no especificas valor del módulo y asume que es 1, siendo == 0 el resultado
+    """
+    Comprueba que la suma en módulo 3 funciona.
+
+    Para ello voy a tomar como argumentos los números 24 y 67.
+    """
+    # para corregirlo unicamente tienes que igualarlo a 0
+    assert modsum(24, 67) == 0
