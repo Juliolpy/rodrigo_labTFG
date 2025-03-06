f86002948c21208a35bcbb81945b408c390fce50
Julio <jcochenta@gmail.com>
2025-03-06 13:04:42 +0100

    creo que está bien pero me sigue saliendo el error de que no enceuntra a src

---
+++ tests/issue_4_2_test.py
@@ -0,0 +1,17 @@
+import pytest
+from src.issue_4_2 import issue_4_2
+import RNA
+
+# definimos la función ya que vamos a probar si funciona en una secuencia que creemos
+# las demás funciones de vienna_RNA están definidas en el otro script que importamos
+
+def test_vienna_RNA():
+
+    test_seq = "AUGCGCGGCGCACACGAGCAU" # secuencia de prueba
+
+    resultado = vienna_RNA(test_seq) # llamamos a la funcion del otro script
+
+    assert resultado["Secuencia"] == "AUGCGCGGCGCACACGAGCAU"
+    assert resultado["secondary_structure"] == "((((.((.......)).))))"
+    assert resultado["min_energy"] == -5.10 
+    assert len(resultado["secondary_structure"]) == len(test_seq)
