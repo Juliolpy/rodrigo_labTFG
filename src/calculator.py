d8fe9bf6c062146253702b5a964879de8391dfd8
Julio <jcochenta@gmail.com>
2025-03-09 21:20:45 +0100

    he mejorado la calculadora, hace multiplicacion y divison, y usa diccionarios con funciones lambda

--- src/calculator.py
+++ src/calculator.py
@@ -6,22 +6,28 @@ def get_parse() -> argparse.Namespace:
 
     parser.add_argument("num1", type= int, help= "Primer número de la operación")
     parser.add_argument("num2", type= int, help= "Segundo número de la operación")
-    parser.add_argument("--Operation", choices=["suma", "resta"], help= "elige que operación realizar: suma o resta")
+    parser.add_argument("--operation", choices=["suma", "resta","multiplicacion", "division"], default= "suma", help= "elige que operación realizar: suma o resta & multiplcación y división")
 
     # ahora almacenamos los valores en la función
     return parser.parse_args()
 
-
-def main() -> None:
+# esta función la definimos para que solo se ejecute si corremos el codigo manualmente, como principal, sin importarlo
+def main() -> None: # no devuelve nada
+    
+    # reclutamos la funcion get_parse y definimos una variable que nos recopile todos los objetos definidos en ella
     args = get_parse()
 
-    if args.Operation == "suma":
-        result = args.num1 + args.num2
-    else:
-        result = args.num1 - args.num2
+    # en vez de usar if statement voy a crear un diccionario con las operaciones:
+    operaciones = {
+        "suma" : lambda x, y: x + y, # lambda es una forma de crear funciones rápidas y anónimas en una sola línea, sin necesidad de usar def
+        "resta" : lambda x, y: x - y,
+        "multiplicacion" : lambda x, y: x * y,
+        "division" : lambda x, y: x / y if y != 0 else "Error: No puedes dividir por 0 meloncio, da infinito y cosas raras"
+    }
+    # agrupamos los valores que obtenemos segun lo que pongamos por la linea de comandos, con el default de suma
+    result = operaciones[args.operation](args.num1, args.num2) # de esta manera argparse no tomará el valor de None si no le especificamos argumento
 
-# sacamos por pantalla
-    print(f"EL resultado de la {args.Operation} es: {result}")
+    print(f"El resultado de tu {args.operation} es : {result}")
 
 if __name__ == "__main__":
     main()
