6ebd14a3b1ca97e2693dcdd51013bcf20615fe25
Julio <jcochenta@gmail.com>
2025-03-18 18:34:29 +0100

    he añadido la opción de generar archivos pickle o json

--- src/cli.py
+++ src/cli.py
@@ -7,6 +7,7 @@ import argparse
 # importamos las funciones del codigo
 from core import read_fasta 
 from core import find_NGG_motivs
+from core import output_NGG_json, output_NGG_pickle # funciones de pickle y json
 
 # definimos nuestra funcion parser
 def get_parse() -> argparse.Namespace:
@@ -16,7 +17,7 @@ def get_parse() -> argparse.Namespace:
 
     # añadimos los argumentos que necesitará nuestro comando de argumentos
     parser.add_argument("fasta", type = str, help="Especique el archivo .fasta a analizar")
-    
+    parser.add_argument("--exit", choices=["json", "pickle"], default= "json", help= "Especifica el tipo de archivo de salida que deseas obtener")
     # guardamos los valores en la funcion
     return parser.parse_args()
 
@@ -30,9 +31,16 @@ def main() -> None:
     # asignamos la función de leer el fasta y encontrar el ngg
     sequences =  read_fasta(args.fasta)
     NGG_positions = find_NGG_motivs(sequences)
+    # especificamos el archivo que queremos
+    if args.exit == "pickle":
+        pickle_file = output_NGG_pickle(NGG_positions)
+    else:
+        json_file = output_NGG_json(NGG_positions)
     # imprimir resultados
     for seq_id, pos in NGG_positions.items():
         print(f"La Secuencia correspondiente con nombre: {seq_id}, tiene {len(pos)} motivos NGG en las posiciones {pos}")
+        # en este caso no ponemos ningun return porque la funcion main() no la vamos a ejecutar en ningun lado y "and" me iba a devolver solo NGG_positions
+        # El operador and en Python devuelve el primer valor falsy que encuentre o, si no hay ninguno, devuelve el último valor evaluado.
 
 # para ejecutar la función como principal y que no de error
 
--- src/core.py
+++ src/core.py
@@ -3,6 +3,8 @@
 # Devuelvan esas posiciones.
 
 # importamos el modulo sys y Seq de Biopython
+import json
+import pickle
 from Bio.Seq import Seq
 from Bio import SeqIO
 
@@ -23,4 +25,25 @@ def find_NGG_motivs(seq_name: dict) -> dict:
       position_list = [i + 1 for i in range(len(seq)-2) if seq[i + 1 : i + 3] == "GG"] # Si es un motivo NGG en la sencuenia
       positions[seq_id] = position_list
    return positions
+
+# funcion para serializar los ouput de las funciones 
+def output_NGG_json(positions: dict) -> dict:
+# JSON
+   # guardamos los datos en json en file
+      with open("output_NGG.json", "w") as file:
+         json.dump(positions, file, indent=4)
+      # los leemos y cargamos
+      with open("output_NGG.json", "r") as file:
+         json_positions = json.load(file)
+
+
+def output_NGG_pickle(positions: dict) -> dict:
+# pickle
+    # guardamos los datos en pickle en file
+   with open("output_NGG.pkl", "wb") as file:
+      pickle.dump(positions, file)
+   # los leemos y cargamos
+   with open("output_NGG.pkl", "rb") as file:
+      pickle_positions = pickle.load(file)
+
 
