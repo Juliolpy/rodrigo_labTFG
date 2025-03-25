9b6727016d6ab6af21fd1bef053cef646aac629a
Julio <jcochenta@gmail.com>
2025-03-25 21:35:54 +0100

    cli.py y core.py actualizados con la clase ColumboParts

--- src/cli.py
+++ src/cli.py
@@ -5,9 +5,7 @@
 # para que estas realicen los cálculos, y luego desde cli.py hay que sacar por pantalla los resultados para que los vea el usuario.
 import argparse
 # importamos las funciones del codigo
-from core import read_fasta 
-from core import find_NGG_motivs
-from core import output_NGG_json, output_NGG_pickle # funciones de pickle y json
+from core import read_fasta, find_NGG_motivs, process_genome, output_NGG_json, output_NGG_pickle
 
 # definimos nuestra funcion parser
 def get_parse() -> argparse.Namespace:
@@ -17,7 +15,7 @@ def get_parse() -> argparse.Namespace:
 
     # añadimos los argumentos que necesitará nuestro comando de argumentos
     parser.add_argument("fasta", type = str, help="Especique el archivo .fasta a analizar")
-    parser.add_argument("--exit", choices=["json", "pickle"], default= "json", help= "Especifica el tipo de archivo de salida que deseas obtener")
+    parser.add_argument("--output", choices=["json", "pickle"], default= "json", help= "Especifica el tipo de archivo de salida que deseas obtener")
     # guardamos los valores en la funcion
     return parser.parse_args()
 
@@ -31,16 +29,33 @@ def main() -> None:
     # asignamos la función de leer el fasta y encontrar el ngg
     sequences =  read_fasta(args.fasta)
     NGG_positions = find_NGG_motivs(sequences)
+    top_candidates = process_genome(sequences, NGG_positions) # vemos cuales son los resultados de la clase y los almacenamos en la variable top_candidates
     # especificamos el archivo que queremos
-    if args.exit == "pickle":
-        pickle_file = output_NGG_pickle(NGG_positions)
+    if args.output == "pickle":
+        pickle_file = output_NGG_pickle(top_candidates)
     else:
-        json_file = output_NGG_json(NGG_positions)
+        json_file = output_NGG_json(top_candidates)
     # imprimir resultados
     for seq_id, pos in NGG_positions.items():
-        print(f"La Secuencia correspondiente con nombre: {seq_id}, tiene {len(pos)} motivos NGG en las posiciones {pos}")
+        print(f"La Secuencia correspondiente con nombre: {seq_id}, tiene {len(pos)} motivos NGG en las posiciones:")
+        print("  ")
+        print(">" * 170)
+        print(f"{pos}")
+        print(("<" * 170))
         # en este caso no ponemos ningun return porque la funcion main() no la vamos a ejecutar en ningun lado y "and" me iba a devolver solo NGG_positions
         # El operador and en Python devuelve el primer valor falsy que encuentre o, si no hay ninguno, devuelve el último valor evaluado.
+        print("\n🔬 Resultados de análisis CRISPR-Cas9:\n")
+        counter = 0
+        for obj in top_candidates:
+            counter += 1
+            print(f"-----ColumboPart nº {counter}-----")
+            print(f" PAM: {obj.PAM}")
+            print(f" Protospacer: {obj.protospacer}")
+            print(f" Localización en el genoma: {obj.position}")
+            print(f" Temperatura de melting (Tm): {obj.tm:.2f}°C")
+            print(f" Scores individuales: {obj.scores}")
+            print(f" Score global: {obj.score_medio:.2f}")
+            print("-" * 50)  # Separador entre cada resultado
 
 # para ejecutar la función como principal y que no de error
 
--- src/core.py
+++ src/core.py
@@ -2,12 +2,55 @@
 # Localicen las posiciones dentro de la secuencia donde tengamos NGG (recuerda que Nsignifica cualquier base).
 # Devuelvan esas posiciones.
 
+# para calcular la melting temperature
+from Bio.SeqUtils import MeltingTemp as mt # mt es la función de la herramienta meltingtemp dentro del modulo sequtils de BIO
 # importamos el modulo sys y Seq de Biopython
 import json
 import pickle
 from Bio.Seq import Seq
 from Bio import SeqIO
 
+# vamos a definir la clase ColumboParts   
+class ColumboParts:
+   # definimos constructor --> debe aceptar un string o un objeto Bio.Seq.Seq
+   def __init__(self, sequence: Seq, position : int): # sequence es un objeto seq y la positicion un numero entero donde se encuentra cada nt
+      self.PAM = sequence[position : position + 3] if position + 3 <= len(sequence) else ""
+      self.protospacer = sequence[position - 20 : position + 3] if position >= 20 else ""
+      self.position = position
+      self.scores = self.calcular_scores ()
+      self.score_medio = sum(self.scores) / len(self.scores) if self.scores else 0 # basicamente la suma de los números que da el return de la funcion scores entre su lenght que siempre será 4
+      self.tm = self.calcular_tm()
+
+      # definimos la función del score
+   def calcular_scores(self):
+      # ponemos la seed_region
+      seed_region = self.protospacer[-12:-4] # cogemos los nucleotidos y nos aseguramos que no coja el PAM
+      # En sistemas CRISPR-Cas9, la región semilla es la parte más crítica para la hibridación entre la guía de ARN y el ADN diana
+      # Esta región está situada cerca del PAM (NGG) y juega un papel clave en la especificidad y estabilidad de la unión -> es donde se calculan los scores
+      # los scores funcionan 1 para verdad y 0 para falso y hacemos una lista con ellos para tenerlos encapsulados
+      scores = [
+         1 if (seed_region.count("G") and seed_region.count("C")) / len(seed_region) < 0.8 else 0, # el contenido de la seed_region de GC ha de ser meno de 80%
+         1 if self.protospacer[-5] == "G" else 0, # que no haya una G antes de la PAM NGG
+         1 if self.protospacer[-7] != "T" else 0, # que ni haya una T 2 posiciones upstream de la PAM 5' -----TXNGG---> 3'
+         1 if not any(x * 3 in seed_region for x in "ATCG") else 0 # que no haya ningun triplete de la misma base, AAA, CCC , TTT , GGG   
+      ]
+      return scores
+      
+   def calcular_tm(self):
+      melting = mt.Tm_NN(self.protospacer) if self.protospacer else 0 # para calcular la tm del protospcaer
+      return melting
+      # transformar el self en diccionairo para qeu pueda ser transfomrado en json
+   def to_dict(self):
+     return {
+        "PAM" : self.PAM,
+        "Protospacer" : self.protospacer,
+        "Position" : self.position,
+        "Scores" : self.scores,
+        "Score_medio" : self.score_medio,
+        "Tm" : self.tm
+     }
+
+# estas funciones behind the musgo, fuera de la clase
 def read_fasta(fastafile: str) -> dict:
    # creamos un diccionario donde guardemos las secuencias
    seq_name = {}
@@ -18,32 +61,43 @@ def read_fasta(fastafile: str) -> dict:
 
 def find_NGG_motivs(seq_name: dict) -> dict:
    # diccionario donde guardamos las posiciones de los motivos NGG
-   positions = {}
+   motifs = {}
 
    for seq_id, seq in seq_name.items():
       # esta línea: i + 1 for i in range(len(seq)-2) --> hace que no nos salgamos del indice 
-      position_list = [i + 1 for i in range(len(seq)-2) if seq[i + 1 : i + 3] == "GG"] # Si es un motivo NGG en la sencuenia
-      positions[seq_id] = position_list
-   return positions
+      motifs_list = [i + 1 for i in range(len(seq)-2) if seq[i + 1 : i + 3] == "GG"] # Si es un motivo NGG en la sencuenia
+      motifs[seq_id] = motifs_list
+   return motifs  
+
+# funcion para procesar el genoma y convertirlo en columboparts
+#  procesa el genoma identificando los sitios donde aparece el motivo NGG y
+#  crea objetos ColumboParts para cada uno, los evalúa y devuelve los 20 mejores candidatos ordenados por su score_medio.
+def process_genome(seq_name: dict, motifs: dict) -> list:
+   columbo_list = []
+   for seq_id, positions in motifs.items():
+      for pos in positions:
+         if pos >= 20: # ya que el protospacer no puede ser menor de 20, nuestro codigo no funcionaria: index out of range
+            columbo_list.append(ColumboParts(seq_name[seq_id], pos)) # objeto seq y un int
+            # ahora para quedarnos con los 20 mejores solo
+            columbo_list.sort(key= lambda x : x.score_medio, reverse= True)
+   return columbo_list[:20]
 
 # funcion para serializar los ouput de las funciones 
-def output_NGG_json(positions: dict) -> dict:
+def output_NGG_json(motifs: dict) -> dict:
 # JSON
    # guardamos los datos en json en file
       with open("output_NGG.json", "w") as file:
-         json.dump(positions, file, indent=4)
+         json.dump([obj.to_dict() for obj in motifs], file, indent=4)
       # los leemos y cargamos
-      with open("output_NGG.json", "r") as file:
+      with open("output_NGG.json", "r") as file: # los cargamos, no hace falta return 
          json_positions = json.load(file)
 
 
-def output_NGG_pickle(positions: dict) -> dict:
+def output_NGG_pickle(motifs: dict) -> dict:
 # pickle
     # guardamos los datos en pickle en file
    with open("output_NGG.pkl", "wb") as file:
-      pickle.dump(positions, file)
+      pickle.dump([obj.to_dict() for obj in motifs], file)
    # los leemos y cargamos
    with open("output_NGG.pkl", "rb") as file:
       pickle_positions = pickle.load(file)
-
-
