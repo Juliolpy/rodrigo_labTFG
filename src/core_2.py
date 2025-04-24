3c7835dc4f82c790255c67c0f2e37a31b4625d8e
Julio <jcochenta@gmail.com>
2025-04-24 16:32:45 +0200

    haciendo el score de primers

--- src/core_2.py
+++ src/core_2.py
@@ -23,13 +23,19 @@ def primer3_design(fastafile: str) -> dict:
       seq_name[seq_id] = seq
    # Creamos el contenido con formato válido
    primer3_input = f"""SEQUENCE_ID=test
-SEQUENCE_TEMPLATE={seq}
+SEQUENCE_TEMPLATE={seq} 
 PRIMER_TASK=generic
 PRIMER_MIN_SIZE=18
 PRIMER_MAX_SIZE=22
 PRIMER_NUM_RETURN=5
 =
 """
+   # ------ df∈[27,42], dr∈[15,100] -------
+   # min = distancia mínima de forward al PAM + tamaño del protospacer + 3 (longitud PAM) + distancia mínima de reverse al PAM
+   # max = distancia máxima de forward al PAM + tamaño del protospacer + 3 + distancia máxima de reverse al PAM
+   # protospacer+PAM ocupa 23 nt, y quieres df∈[27,42], dr∈[15,100], entonces:
+   # PRIMER_PRODUCT_SIZE_RANGE= (27 + 23 + 15, 42 + 23 + 100) = (65, 165)
+
    with open("entry.txt", "w") as f:
       f.write(primer3_input)
 
@@ -47,6 +53,63 @@ PRIMER_NUM_RETURN=5
    # procesamos dicho str y lo convertimos en un diccionario
 
    return output
+
+
+def score_primers(output: dict, pam_pos: int, protospacer_len: int) -> float:
+   """
+   Función que evalua la bondad de los primers y las posiciones en las que tienen que unirse
+   Dado el dict parseado de Primer3 y la posición del PAM, devuelve un score 0 - 1 combinando:
+
+      - Cumplimiento de distancias
+
+      - Contenido GC (~50 %)
+
+      - Tm (proporcionado por Primer3)
+
+   :param output: dict con los primers que saca primer3
+   :type output: dict
+   :param pam_pos: posicion de las PAM evaluadas por Columbo
+   :type pam_pos: int
+   :param protospacer_len: longitud del protospacer + PAM (23 nt)
+   :type protospacer_len: int
+
+   :return: score de primers
+   :rtype: float
+   
+   output:
+   - 'PRIMER_LEFT_0': (start, length)
+   - 'PRIMER_RIGHT_0': (start, length)
+   - 'PRIMER_LEFT_0_TM', 'PRIMER_RIGHT_0_TM'
+   - 'PRIMER_LEFT_0_GC_PERCENT', 'PRIMER_RIGHT_0_GC_PERCENT'
+   pam_pos = índice 0-based donde empieza el PAM en la secuencia.
+   protospacer_len = longitud de protospacer+PAM (por defecto 23).
+   
+   """
+   # posiciones
+   left_start, left_len = output["PRIMER_LEFT_0"]
+   right_start, right_len = output["PRIMER_RIGHT_0"]
+   # (1) calculamos distacias 
+   df = pam_pos - left_start
+   dr = (right_start + right_len - 1) - (pam_pos + protospacer_len - 1)
+   # (2) puntuación por distancia (1 si esta en el rango y 0 si no)
+   s_df = 1.0 if 27 <= df <= 42 else 0.0
+   s_dr = 1.0 if 15 <= dr <= 100 else 0.0
+   # (3) puntuación por contenido GC (máx ->50 %), la da el programa primer3
+   def gc_score(gc_percent):
+      return 1.0 - abs(gc_percent - 50)/50 # 1.0 si gc = 50, decae linealmente
+   s_gc = ((gc_score(output['PRIMER_LEFT_0_GC_PERCENT'])) + (gc_score(output["PRIMER_RIGHT_0_GC_PERCENT"]))) / 2
+   # (4) puntuación por la tm, la da el programa -> ideal 58 - 62 ºC
+   def tm_score(tm):
+      if 58 <= tm <= 62:
+         return 1.0
+      return max(0, 1 - min(abs(tm-60)/10, 1)) # 60 al ser la media entre 58 y 62, entre 10 para escalarlo entre 0 y 1 y que no pase de 1. Funciones anidadas
+   s_tm = ( tm_score(output['PRIMER_LEFT_0_TM']) + tm_score(output['PRIMER_RIGHT_0_TM']) ) / 2
+
+   # score global, dandole más importancia a la distancia
+   score_global = 0.5 * (s_df * s_dr) + 0.25 * s_gc + 0.25 * s_tm
+   return score_global # a la funcion score_primers
+
+
 
 # funcion parser para gestionar las salidas del programa primer3
 def parse_primers_output(output: str) -> dict:
